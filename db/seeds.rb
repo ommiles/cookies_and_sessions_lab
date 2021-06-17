62a5182de2e876a311cb5ee318e4db2c4864b643
OM Miles <miles@ommiles.com>
2021-06-16 18:48:33 -0700

    commit tree 6bf0e9bf7ae6d0ce605a3d25dda772b1327b93a3

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+5.times do
+    1.times do
+        veggie = Faker::Food.vegetables
+        Product.create(name: veggie, price: rand(1...15))
+    end
+    
+    1.times do
+        fruit = Faker::Food.fruits
+        Product.create(name: fruit, price: rand(1...15))
+    end
+end
